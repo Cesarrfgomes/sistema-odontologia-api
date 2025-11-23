0992c4e43317882e450d5467df22c9e6b36f3de9
César Filipe <filipegomes012642@gmail.com>
2025-11-22 22:53:54 -0300

    feat: criando relacionamento entre cobrança e plano de pagamento

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,7 @@ import { healthCheckRoute } from './http/routes/health-check.ts'
 import { createPatient } from './http/routes/patient/create.ts'
 import { createPaymentMethod } from './http/routes/payment/create-method.ts'
 import { createPaymentPlan } from './http/routes/payment/create-plan.ts'
+import { createPaymentRelation } from './http/routes/payment/create-relation.ts'
 import { createProcedure } from './http/routes/procedure/create.ts'
 import { createProcedureCategory } from './http/routes/procedure/create-category.ts'
 import { auth } from './http/routes/user/auth.ts'
@@ -59,6 +60,7 @@ app.register(createProcedureCategory)
 app.register(createProcedure)
 app.register(createPaymentMethod)
 app.register(createPaymentPlan)
+app.register(createPaymentRelation)
 
 app.listen({ port: env.PORT }).then(() => {
 	console.log(`HTTP server is running on port ${process.env.PORT}`)
